7a1f6f50af9f228eb04ad5122bd74d1f6eb02802
Arul <me@arulraj.net>
2024-12-23 00:44:55 +0530

    base files added for standalone login page
    - login form serve from expressjs
    
    - static files also served

--- built-in-login-page/README.md
+++ built-in-login-page/README.md
@@ -24,3 +24,12 @@ After successful login, you will be redirected to the service.
 To know the headers passed to the app - http://localhost/whoami
 To Get the user information - http://localhost/auth/userinfo
 To Logout - http://localhost/auth/logout
+
+
+## Cleanup
+
+To bring down the stack
+
+```bash
+docker compose down
+```
---
+++ standalone-login-page/app/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  var userName = req.header('X-Auth-Request-User') || 'Guest';
+  console.log(req.headers);
+  userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase();
+  res.send(`
+    <h1>Welcome ${userName},</h1>
+    <br> This is the protected page.
+    <br> Only authenticated users can access this page.
+    <br> <a href="/whoami">To see headers?</a>
+    <br> <a href="/auth/sign_out">Logout</a>
+    `);
+});
+
+app.get('/login', (req, res) => {
+  res.send(`
+    <head>
+      <title>Login Page</title>
+      <style src="/static/style.css"></style>
+    </head>
+    <h1>Login Page</h1>
+    <form action="/auth/sign_in" method="post">
+      <input type="hidden" name="rd" value="/">
+      <label for="username">Username:</label><br>
+      <input type="text" id="username" name="username"><br><br>
+      <label for="password">Password:</label><br>
+      <input type="password" id="password" name="password"><br><br>
+      <input type="submit" value="Login">
+    </form>
+    `);
+});
+
+// Expose static files
+app.use(express.static('static'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
